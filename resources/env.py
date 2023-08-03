082d2d827f8ed4e01221f57a9633383c458b8f88
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-08-03 18:57:13 +0300

    add main links and product names

---
+++ resources/env.py
@@ -0,0 +1,9 @@
+class Resources:
+    TIMEOUT = 20
+    MAIN_LINK = "https://demo.nopcommerce.com/"
+    REGISTER_LINK = "https://demo.nopcommerce.com/register?returnUrl=%2F"
+
+    PC_PAGE_NAME = "Build your own computer"
+    MACBOOK_PAGE_NAME = "Apple MacBook Pro 13-inch"
+    GIFT_PAGE_NAME = "$25 Virtual Gift Card"
+    PHONE_PAGE_NAME = "HTC One M8 Android L 5.0 Lollipop"
