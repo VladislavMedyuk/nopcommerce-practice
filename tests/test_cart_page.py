e1c28ac83e18d14ec02e06a8e1c721595c25c8f0
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-08-03 20:01:44 +0300

    add check to go to cart and check product in cart

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -1,4 +1,6 @@
+from pages.cart_page import CartPage
 from pages.main_page import MainPage
+from pages.product_page import ProductPage
 from resources.env import Resources
 
 
@@ -7,3 +9,10 @@ class TestCartPage:
         main_page = MainPage(browser, Resources.MAIN_LINK)
         main_page.open()
         main_page.should_be_shopping_cart()
+
+    def test_user_can_go_to_shopping_cart(self, browser):
+        main_page = MainPage(browser, Resources.MAIN_LINK)
+        main_page.open()
+        main_page.go_to_shopping_cart()
+        cart_page = CartPage(browser, browser.current_url)
+        cart_page.cart_should_be_empty()
