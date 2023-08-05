ed44d551f011da5f6e49d6c03add33a0348b29ca
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-08-05 22:40:46 +0300

    add test case for user registration

--- tests/test_register_user.py
+++ tests/test_register_user.py
@@ -4,4 +4,8 @@ from resources.env import Resources
 
 class TestRegisterNewUser:
     def test_register_new_user(self, browser):
-        pass
+        register_page = RegisterPage(browser, Resources.REGISTER_LINK)
+        register_page.open()
+        email, first_name, last_name, company, password = register_page.generate_random_user()
+        register_page.register_new_user(email, first_name, last_name, company, password)
+        register_page.should_be_success_registration()
