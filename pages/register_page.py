893a2791a93ee65d991b95027c7e3b7ff46e1aa4
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-08-05 22:34:43 +0300

    add method for generating random data and filling registration form

--- pages/register_page.py
+++ pages/register_page.py
@@ -1,5 +1,33 @@
+from faker import Faker
+
+from locators.locators import RegisterPageLocators
 from pages.base_page import BasePage
 
 
 class RegisterPage(BasePage):
-    pass
+    @staticmethod
+    def generate_random_user() -> tuple:
+        faker = Faker()
+        email = faker.email()
+        first_name = faker.first_name()
+        last_name = faker.last_name()
+        password = faker.password()
+        company = faker.company()
+        return email, first_name, last_name, company, password
+
+    def register_new_user(self, email, first_name, last_name, company, password):
+        gender = self.find_element(RegisterPageLocators.GENDER)
+        gender.click()
+        first_name_bar = self.find_element(RegisterPageLocators.FIRST_NAME)
+        first_name_bar.send_keys(first_name)
+        last_name_bar = self.find_element(RegisterPageLocators.LAST_NAME)
+        last_name_bar.send_keys(last_name)
+        email_bar = self.find_element(RegisterPageLocators.EMAIL)
+        email_bar.send_keys(email)
+        company_bar = self.find_element(RegisterPageLocators.COMPANY)
+        company_bar.send_keys(company)
+        passwords = self.find_elements(RegisterPageLocators.PASSWORD)
+        for parole in passwords:
+            parole.send_keys(password)
+        register_button = self.find_element(RegisterPageLocators.REGISTER_BUTTON)
+        register_button.click()
