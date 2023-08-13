158769781f7c08e21dc54fedc3bc43495352ab58
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-08-13 16:19:35 +0300

    add keywords for search

--- resources/env.py
+++ resources/env.py
@@ -8,3 +8,7 @@ class Resources:
     MACBOOK_PAGE_NAME = "Apple MacBook Pro 13-inch"
     GIFT_PAGE_NAME = "$25 Virtual Gift Card"
     PHONE_PAGE_NAME = "HTC One M8 Android L 5.0 Lollipop"
+
+    EXISTING_PRODUCT = "phone"
+    NONEXISTENT_PRODUCT = "machine"
+    EMPTY_REQUEST = ""
