5f7212969631be2e208eb371ac28033e3a285c70
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-08-13 17:40:16 +0300

    add new test cases

--- tests/test_search_bar.py
+++ tests/test_search_bar.py
@@ -11,4 +11,22 @@ def test_search_for_an_existing_product(browser):
     search_page = SearchPage(browser, browser.current_url)
     search_page.should_be_search_results()
     search_page.result_should_contain_information_about_the_products()
-    search_page.products_should_have_name_price_and_image()
+    # search_page.products_should_have_name_price_and_image()
+    search_page.search_results_should_be_unique()
+
+
+def test_search_for_a_nonexistent_product(browser):
+    main_page = MainPage(browser, Resources.MAIN_LINK)
+    main_page.open()
+    main_page.should_be_search_bar()
+    main_page.search_for_a_product(Resources.NONEXISTENT_PRODUCT)
+    search_page = SearchPage(browser, browser.current_url)
+    search_page.should_be_message_about_the_absence_of_products()
+
+
+def test_search_with_an_empty_request(browser):
+    main_page = MainPage(browser, Resources.MAIN_LINK)
+    main_page.open()
+    main_page.should_be_search_bar()
+    main_page.search_for_a_product(Resources.EMPTY_REQUEST)
+    main_page.should_be_a_popup_alert()
