201820d6f4108bd3944a0cd00141f808c8595b8a
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-09-03 15:22:03 +0300

    add allure reports

--- pages/search_page.py
+++ pages/search_page.py
@@ -1,12 +1,16 @@
+import allure
+
 from locators.locators import SearchPageLocators
 from pages.base_page import BasePage
 
 
 class SearchPage(BasePage):
+    @allure.step("Проверяем есть ли результаты поиска на странице")
     def should_be_search_results(self):
         assert self.elements_are_present(
             SearchPageLocators.PRODUCT_ITEMS), "The search results are not displayed, but they should"
 
+    @allure.step("Проверяем что результаты поиска содержат информацию о продукте")
     def result_should_contain_information_about_the_products(self):
         assert self.element_is_present(
             SearchPageLocators.PRODUCT_TITLE), "Product information is not displayed, but it should be on the page"
@@ -18,6 +22,7 @@ class SearchPage(BasePage):
             assert result.find_element(SearchPageLocators.PRODUCT_NAME), "2"
             assert result.find_element(SearchPageLocators.PRODUCT_PRICE), "3"
 
+    @allure.step("Проверяем, что все результаты поиска уникальны")
     def search_results_should_be_unique(self):
         product_elements = self.find_elements(SearchPageLocators.PRODUCT_NAME)
         names = []
@@ -25,6 +30,7 @@ class SearchPage(BasePage):
             names.append(product.text)
         assert len(names) == len(set(names)), "The product name is repeated, but it should not"
 
+    @allure.step("Проверяем результат поиска на предмет отсутствия товаров")
     def should_be_message_about_the_absence_of_products(self):
         assert self.element_is_present(
             SearchPageLocators.ERROR_MESSAGE), "There are some product on the search page, but they shouldn't"
--- tests/test_search_bar.py
+++ tests/test_search_bar.py
@@ -1,8 +1,11 @@
+import allure
+
 from pages.main_page import MainPage
 from pages.search_page import SearchPage
 from resources.env import Resources
 
 
+@allure.step("Тест: Поиск существующего товара")
 def test_search_for_an_existing_product(browser):
     main_page = MainPage(browser, Resources.MAIN_LINK)
     main_page.open()
@@ -15,6 +18,7 @@ def test_search_for_an_existing_product(browser):
     search_page.search_results_should_be_unique()
 
 
+@allure.step("Тест: Поиск несуществующего товара")
 def test_search_for_a_nonexistent_product(browser):
     main_page = MainPage(browser, Resources.MAIN_LINK)
     main_page.open()
@@ -24,6 +28,7 @@ def test_search_for_a_nonexistent_product(browser):
     search_page.should_be_message_about_the_absence_of_products()
 
 
+@allure.step("Тест: Поиск с пустым запросом")
 def test_search_with_an_empty_request(browser):
     main_page = MainPage(browser, Resources.MAIN_LINK)
     main_page.open()
